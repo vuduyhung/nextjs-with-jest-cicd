34189e71884e9fd47a08aed443e85eecc6c8f526
Hung Vu <vn.vuduyhung@gmail.com>
2024-10-10 00:20:40 +0700

    add another change on develop

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         />
         <h1>This is a demo Nextjs 14.2.15 page about test with jest then run github CI/CD</h1>
         <p>This is another description test</p>
+        <p>Description</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
