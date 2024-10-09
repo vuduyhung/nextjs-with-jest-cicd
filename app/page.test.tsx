60e7db1a0e89e01c9e84e4ce154ccdcc267f42ca
Hung Vu <vn.vuduyhung@gmail.com>
2024-10-09 23:33:44 +0700

    Init project

---
+++ app/page.test.tsx
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+it("App Router: Works with Server Components", () => {
+  render(<Page />);
+
+  const image = screen.getByRole("img", { name: /next\.js logo/i });
+  expect(image).toHaveAttribute("src", "https://nextjs.org/icons/next.svg");
+});
+
+it("Page must have 'This is demo page' text", () => {
+  render(<Page />);
+
+  const pageText = screen.getByText(/This is a demo Nextjs 14.2.15/i);
+  expect(pageText).toBeInTheDocument();
+});
+
+// it("A sample error test to make test fail", () => {
+//   render(<Page />);
+
+//   const pageText = screen.getByText(/This is a xxx Hello!/i);
+//   expect(pageText).toBeInTheDocument();
+// });
